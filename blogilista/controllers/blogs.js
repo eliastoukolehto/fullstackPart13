44bd996e1664301b53c8b3b1cab6ac72e55c8bbd
elias <eliastoukolehto@gmail.com>
2025-02-23 16:09:36 +0200

    ex 13.4

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,47 +1,69 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
 const jwt = require('jsonwebtoken')
 const { userExtractor } = require('../utils/middleware')
 const user = require('../models/user')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+Blog.sync()
+
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-      .find({})
-      .populate('user', { username: 1, name: 1 })
+    const blogs = await Blog.findAll()
     response.json(blogs)
 })
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
 
   const body = request.body
-  const user = request.user
-  
-  const blog = await new Blog({
+  console.log(body)
+
+  savedBlog = await Blog.create({
     author: body.author,
     title: body.title,
     url: body.url,
-    likes: body.likes,
-    user: user._id
+    likes: body.likes
   })
-  const savedBlog = await blog.save()
 
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).json(savedBlog)
+  response.status(201).json(savedBlog) 
 })
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
   const user = request.user
 
-  const blog = await Blog.findById(request.params.id)
-  if ( blog.user.toString() === user._id.toString() ) {
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-  } else {
-    response.status(401).json({ error: 'unauthorized user' })
-  }
+  const blog = await Blog.findByPk(request.params.id)
 
+  await blog.destroy()
+  response.status(204).end()
   
 })
 
